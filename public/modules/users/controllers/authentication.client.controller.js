f281b1b7ab0f1a0d98f0005162e75f66d82473bf
Stanley Huynh <imstanleyhuynh@gmail.com>
2015-07-18 19:18:24 -0400

    cleaned up shit

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -8,7 +8,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		if ($scope.authentication.user) $location.path('/');
 
 		$scope.signup = function() {
-			console.log($scope.credentials);
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
